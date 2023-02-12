f6c2084efd91ba87a698017dbac73396f13739ef
SikkeDotExe <sikkerg@protonmail.com>
2023-02-12 09:36:40 -0500

    First

---
+++ user/main.cpp
@@ -0,0 +1,154 @@
+// Generated C++ file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
+// Custom injected code entry point
+
+#include "pch-il2cpp.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <iostream>
+#include "il2cpp-appdata.h"
+#include "helpers.h"
+#include "il2cpp-init.h"
+
+using namespace app;
+
+// Set the name of your log file here
+extern const LPCWSTR LOG_FILE = L"il2cpp-log.txt";
+
+// Custom injected code entry point
+void Run()
+{
+    // Initialize thread data - DO NOT REMOVE
+	init_il2cpp();
+    il2cpp_thread_attach(il2cpp_domain_get());
+
+    // If you would like to write to a log file, specify the name above and use il2cppi_log_write()
+    // il2cppi_log_write("Startup");
+
+    // If you would like to output to a new console window, use il2cppi_new_console() to open one and redirect stdout
+    il2cppi_new_console();
+
+    // Place your custom code here
+}
+
+#include "pch-il2cpp.h"
+#pragma once
+#include <Windows.h>
+#include <d3d11.h>
+#include <dxgi.h>
+#include "../kiero/kiero.h"
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_win32.h"
+#include "../imgui/imgui_impl_dx11.h"
+
+typedef HRESULT(__stdcall* Present) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
+typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
+typedef uintptr_t PTR;
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+Present oPresent;
+HWND window = NULL;
+WNDPROC oWndProc;
+ID3D11Device* pDevice = NULL;
+ID3D11DeviceContext* pContext = NULL;
+ID3D11RenderTargetView* mainRenderTargetView;
+
+void InitImGui()
+{
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX11_Init(pDevice, pContext);
+}
+
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+bool init = false;
+namespace globals {
+	GameObject* grannyObject;
+	EnemyAIGranny* enemyAI;
+	Transform* grannyTransform;
+	Vector3 grannyPosition;
+}
+HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
+{
+	if (!init)
+	{
+		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&pDevice)))
+		{
+			pDevice->GetImmediateContext(&pContext);
+			DXGI_SWAP_CHAIN_DESC sd;
+			pSwapChain->GetDesc(&sd);
+			window = sd.OutputWindow;
+			ID3D11Texture2D* pBackBuffer;
+			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+			pDevice->CreateRenderTargetView(pBackBuffer, NULL, &mainRenderTargetView);
+			pBackBuffer->Release();
+			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+			InitImGui();
+			init = true;
+			Run();
+			//il2cpp_thread_attach(il2cpp_domain_get()); // initialize
+			//il2cppi_new_console();
+			
+			//globals::grannyObject = GameObject_Find(reinterpret_cast<String*>(il2cpp_string_new("GrannyParent")), nullptr);
+			//globals::enemyAI = reinterpret_cast<EnemyAIGranny*>(GameObject_GetComponentByName(globals::grannyObject, reinterpret_cast<String*>(il2cpp_string_new("EnemyAIGranny")), nullptr));
+			//std::cout << "Fields " << globals::enemyAI->fields.walkSpeed << std::endl;
+			
+		}
+
+		else
+			return oPresent(pSwapChain, SyncInterval, Flags);
+	}
+
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("ImGui Window");
+	
+	ImGui::End();
+
+	ImGui::Render();
+
+	pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+	return oPresent(pSwapChain, SyncInterval, Flags);
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool init_hook = false;
+	
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
+		{
+			kiero::bind(8, (void**)&oPresent, hkPresent);
+			init_hook = true;
+		}
+	} while (!init_hook);
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
+}
